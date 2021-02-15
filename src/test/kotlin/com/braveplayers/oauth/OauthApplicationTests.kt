1c8d45d54c2458a4ce833ac9c16713ea8ee182a5
Tiago Camilo <camilo.tiago@gmail.com>
2021-02-15 10:47:56 -0300

    Initial commit

---
+++ src/test/kotlin/com/braveplayers/oauth/OauthApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.braveplayers.oauth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OauthApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
